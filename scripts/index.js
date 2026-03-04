16d1f649203cb54ed000dd5f18afeeecb5676a7e
Rintia Islam <rintiaislam110@gmail.com>
2026-03-04 10:00:28 +0600

    functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,43 @@ const loadLessons =() =>{
     .then(json => displayLessons(json.data))
 }
 
+const loadLevelWords =(id) =>{
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWords(data.data))
+}
+
+const displayLevelWords =(words) =>{
+    console.log(words);
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML ='';
+
+    words.forEach(word => {
+        card = document.createElement('div');
+        card.innerHTML = `
+         <div class="bg-white py-10 px-5 rounded-lg shadow-sm text-center space-y-4">
+            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+            <div class="font-bangla text-2xl">" ${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `
+        wordContainer.append(card)
+    });
+}
+
+//  {
+//     "id": 5,
+//     "level": 1,
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার"
+// }
+
 const displayLessons =(lessons) =>{
     
     const levelContainer = document.getElementById('level-container');
@@ -13,7 +50,7 @@ const displayLessons =(lessons) =>{
         const btnDiv = document.createElement('div')
 
         btnDiv.innerHTML =`
-        <button class="btn btn-outline btn-primary">
+        <button onclick= 'loadLevelWords(${lesson.level_no})' class="btn btn-outline btn-primary">
         <i class="fa-solid fa-book-open"></i>Level -${lesson.level_no}</button>
         `
     levelContainer.append(btnDiv)
